05e67e41021635edc7801796327a6ce1a519c4b2
Hannah Matsue Mills Hironaka <hannahh@myth21.Stanford.EDU>
2013-01-21 11:31:03 -0800

    Fixed bug with webserver.

--- bufferbloat.py
+++ bufferbloat.py
@@ -147,7 +147,7 @@ def get_latency_stats(net):
     while True:
         # Calculate the amount of time to transfer webpage. TODO: update 
         output = "latency.txt"
-        cmd = "curl -o index.html -s -w %%{time_total} %s/http/index.html > %s" % (client.IP(), output)
+        cmd = "curl -o index.html -s -w %%{time_total} %s/http/index.html > %s" % (server.IP(), output)
         print cmd
         client.popen(cmd, shell=True)
         f = open(output)
