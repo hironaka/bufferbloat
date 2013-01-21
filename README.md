3553651c4eca2b83bf794285e4c004502d2efeee
Hannah Matsue Mills Hironaka <hannahh@myth14.Stanford.EDU>
2013-01-21 13:47:18 -0800

    Updated readme and run script to correct specifications.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 bufferbloat
 ===========
+
+1. TCP congestion control attempts have full utilization of the links by filling the 
+increasing the window size additively until a packet is dropped. A packet will not be dropped 
+the queue fills up, and a larger. The queue will already be filled up by the first TCP
+flow. Any subsequent flow will have to wait on these queues to empty.
+
+2. 1000 max queue size
+1500 byte MTU
+size in byte = 1500kB
+100 Mb/s
+
+1500 * 1000 * 8 / 100 * 10^6
+
+0.12 seconds
+
+3.
+RTT = 6 * queuesize
+
+4.
+qualtiy of service with multiple queues
+active queue management
--- run.sh
+++ run.sh
@@ -14,13 +14,11 @@ iperf_port=5001
 for qsize in 20 100; do
     dir=bb-q$qsize
 
-    # Run bufferbloat.py here...
+    # Run bufferbloat.py...
 		python bufferbloat.py -b 1.5 -D 5 -d $dir -t 200 -q $qsize
 		
-    # TODO: Ensure the input file names match the ones you use in
-    # bufferbloat.py script.  Also ensure the plot file names match
-    # the required naming convention when submitting your tarball.
-    python plot_tcpprobe.py -f $dir/cwnd.txt -o $dir/cwnd-iperf.png -p $iperf_port
-    python plot_queue.py -f $dir/q.txt -o $dir/q.png
-    python plot_ping.py -f $dir/ping.txt -o $dir/rtt.png
+    # Output graphs...
+    python plot_tcpprobe.py -f $dir/cwnd.txt -o cwnd-q$qsize.png -p $iperf_port
+    python plot_queue.py -f $dir/q.txt -o buffer-q$qsize.png
+    python plot_ping.py -f $dir/ping.txt -o rtt-q$qsize.png
 done
